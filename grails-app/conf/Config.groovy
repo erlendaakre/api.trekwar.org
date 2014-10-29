088aef9929885e2c4f103322d12b2ab667e01d18
Erlend Aakre <erlend.aakre@aakretech.com>
2014-10-29 08:04:48 +0000

    removed unused config file

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -116,7 +116,7 @@ grails {
         host = "smtp.gmail.com"
         port = 465
         username = "trekwaronline@gmail.com"
-        password = "______" // TODO: update before deploy
+        password = "" 
         props = ["mail.smtp.auth":"true",
                  "mail.smtp.socketFactory.port":"465",
                  "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
