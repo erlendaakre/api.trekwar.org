d79e512bd119a7b42993cb8af93cc253556e19d4
Erlend Aakre <erlend.aakre@aakretech.com>
2014-10-29 07:46:09 +0000

    forgot to remove gmail password again :)
    At least that gmail password gets changed more often than my underwear

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -116,7 +116,7 @@ grails {
         host = "smtp.gmail.com"
         port = 465
         username = "trekwaronline@gmail.com"
-        password = "dZknJ5TWRcetWfRD4pndIVay0" // TODO: update before deploy
+        password = "______" // TODO: update before deploy
         props = ["mail.smtp.auth":"true",
                  "mail.smtp.socketFactory.port":"465",
                  "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
